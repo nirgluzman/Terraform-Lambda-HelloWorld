dd7ffb91a92138df1763179e8a92ff847a2a2a76
nirgluzman <nir.gluzman@gmail.com>
2024-04-12 18:44:27 +0200

    detect code change by watching the zip hash

--- main.tf
+++ main.tf
@@ -59,9 +59,9 @@ resource "aws_cloudwatch_log_group" "lambda_loggroup" {
 
 # Generates an archive from content, a file, or a directory of files.
 data "archive_file" "zip_the_js_code" {
- type        = "zip"
- source_dir  = "${path.module}/js/"
- output_path = "${path.module}/js/hello.zip"
+  type        = "zip"
+  source_dir  = "${path.module}/js/"
+  output_path = "${path.module}/js/hello.zip"
 }
 
 # Create a lambda function
@@ -73,7 +73,7 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   function_name   = "Terraform-Lambda-Function"
   role            = aws_iam_role.lambda_role.arn
   handler         = "hello.handler" # (Optional) Function entrypoint in your code
-  source_code_hash = filebase64sha256("${path.module}/js/hello.mjs") # detect changes in lambda source code
+  source_code_hash = data.archive_file.zip_the_js_code.output_base64sha256 # detect changes in lambda zip file
   runtime         = "nodejs18.x"
 
   environment {   # Environment variables
